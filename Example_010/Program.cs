9ce5cd0dcfd21651942bed4ef04c8c5c3564998b
Ilya <ackulin.ilya@yandex.ru>
2022-06-30 19:51:36 +0300

    Добаили программу которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа

---
+++ Example_010/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Clear();
+System.Console.WriteLine();
+int number = new Random().Next(100, 1000);
+
+System.Console.WriteLine($"Второй цифрой числа {number} является {number / 10 % 10} ");
+
+System.Console.WriteLine(); 
